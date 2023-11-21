9498d73691aca8e0a4a0456fdd43bfafa1faf6c2
Salim <sidymamadousalim@gmail.com>
2023-11-21 20:22:06 +0100

    remice dburl

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string().url().optional(),
     NEXTAUTH_SECRET: z.string(),
     GITHUB_SECRET: z.string(),
     GITHUB_ID: z.string(),
