ac1322151128c7575e6652604269a77369dc1e45
Salim <sidymamadousalim@gmail.com>
2023-11-22 20:16:31 +0100

    remove 5

--- components/quizz/GenerateAiQuestion.tsx
+++ components/quizz/GenerateAiQuestion.tsx
@@ -60,81 +60,71 @@ const GenerateAiQuestions = ({ countMax }: { countMax: number }) => {
   // }
 
   return (
-    <TextArea
-      disabled={countMax <= 0}
-      max={maxLength}
-      count={textAreaCount}
-      onChange={(e) => {
-        e.preventDefault();
-        setTextAreaCount(e.currentTarget.value.length);
+    <form
+      onSubmit={async (e) => {
+        await handleSubmit(e).then(async () => {
+          await decrementNumberAction();
+        });
       }}
-      name="user"
-      label="Entrez le texte"
-    />
-    // <form
-    //   onSubmit={async (e) => {
-    //     await handleSubmit(e).then(async () => {
-    //       await decrementNumberAction();
-    //     });
-    //   }}
-    // >
-    //   <h3 className="py-4 font-bold text-lg">
-    //     Generer un examen à partir d&apos;un texte donné
-    //   </h3>
+    >
+      <h3 className="py-4 font-bold text-lg">
+        Generer un examen à partir d&apos;un texte donné
+      </h3>
 
-    //   {countMax <= 0 && !GenereQuestionsWithAi.isPending && (
-    //     <p className="text-red-500 font-bold">
-    //       Les nombres de possiblités sont terminés !!
-    //     </p>
-    //   )}
+      {/* {countMax <= 0 && !GenereQuestionsWithAi.isPending && (
+        <p className="text-red-500 font-bold">
+          Les nombres de possiblités sont terminés !!
+        </p>
+      )} */}
 
-    //   {GenereQuestionsWithAi.isPending ? (
-    //     <div className="w-full flex justify-center items-center h-36">
-    //       <div className="flex flex-col justify-center items-center">
-    //         <p>Le systeme est entrain de generer...</p>
-    //         <span className="loading loading-infinity loading-lg"></span>
-    //       </div>
-    //     </div>
-    //   ) : (
-    //     <fieldset name="user" className="flex items-end gap-2 flex-col ">
-    //       <div className="w-full">
-    //         <TextArea
-    //           disabled={countMax <= 0}
-    //           max={maxLength}
-    //           count={textAreaCount}
-    //           onChange={(e) => {
-    //             e.preventDefault();
-    //             setTextAreaCount(e.currentTarget.value.length);
-    //           }}
-    //           name="user"
-    //           label="Entrez le texte"
-    //         />
-    //         <p
-    //           className={clsx("text-end my-4", {
-    //             "text-red-400": textAreaCount > maxLength,
-    //           })}
-    //         >
-    //           {textAreaCount} / {maxLength}
-    //         </p>
-    //       </div>
-    //       <Button
-    //         disabled={
-    //           GenereQuestionsWithAi.isPending ||
-    //           textAreaCount > maxLength ||
-    //           countMax <= 0
-    //         }
-    //         className="bg-green-500"
-    //         type="submit"
-    //       >
-    //         {GenereQuestionsWithAi.isPending ? (
-    //           <p>le systeme est en train de reflechir</p>
-    //         ) : (
-    //           "Generer"
-    //         )}
-    //       </Button>
-    //     </fieldset>
-    //   )}
-    // </form>
+      {/* {GenereQuestionsWithAi.isPending ? ( */}
+      {true ? (
+        <div className="w-full flex justify-center items-center h-36">
+          <div className="flex flex-col justify-center items-center">
+            <p>Le systeme est entrain de generer...</p>
+            <span className="loading loading-infinity loading-lg"></span>
+          </div>
+        </div>
+      ) : (
+        <fieldset name="user" className="flex items-end gap-2 flex-col ">
+          <div className="w-full">
+            <TextArea
+              disabled={countMax <= 0}
+              max={maxLength}
+              count={textAreaCount}
+              onChange={(e) => {
+                e.preventDefault();
+                setTextAreaCount(e.currentTarget.value.length);
+              }}
+              name="user"
+              label="Entrez le texte"
+            />
+            <p
+              className={clsx("text-end my-4", {
+                "text-red-400": textAreaCount > maxLength,
+              })}
+            >
+              {textAreaCount} / {maxLength}
+            </p>
+          </div>
+          <Button
+            disabled={
+              // GenereQuestionsWithAi.isPending ||
+              textAreaCount > maxLength || countMax <= 0
+            }
+            className="bg-green-500"
+            type="submit"
+          >
+            {/* {GenereQuestionsWithAi.isPending ? (
+              <p>le systeme est en train de reflechir</p>
+            ) : (
+              "Generer"
+            )} */}
+            generer
+          </Button>
+        </fieldset>
+      )}
+    </form>
   );
 };
 
